7193ccc87d01a5660973744665e59f26dd593ccc
leader2869 <94763336+leader2869@users.noreply.github.com>
2025-11-10 18:00:03 +0300

    Delete src directory

--- src/database/add-club-isvalidated-field.sql
+++
@@ -1,9 +0,0 @@
--- Добавляем поле isValidated в таблицу clubs
-ALTER TABLE clubs
-ADD COLUMN IF NOT EXISTS "isValidated" BOOLEAN NOT NULL DEFAULT false;
-
--- Устанавливаем isValidated = true для существующих клубов (они уже были созданы до введения валидации)
-UPDATE clubs
-SET "isValidated" = true
-WHERE "isValidated" = false;
-
--- src/database/add-club-rejectioncomment-field.sql
+++
@@ -1,9 +0,0 @@
--- Добавляем поле rejectionComment в таблицу clubs
-ALTER TABLE clubs
-ADD COLUMN IF NOT EXISTS "rejectionComment" TEXT;
-
--- Обновляем существующие записи, устанавливая rejectionComment в NULL
-UPDATE clubs
-SET "rejectionComment" = NULL
-WHERE "rejectionComment" IS NULL;
-
