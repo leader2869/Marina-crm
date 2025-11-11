957d62f9fe487474cb758a7d74982d38fe7a85f7
MacBook <admin@MacBook-Pro-Vitalij.local>
2025-11-11 14:20:22 +0300

    Исправлен отступ для блока кнопок в модальном окне создания яхт-клуба

--- client/src/pages/Clubs.tsx
+++ client/src/pages/Clubs.tsx
@@ -979,7 +979,7 @@ export default function Clubs() {
                   </div>
                 </div>
 
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
                   onClick={handleCreate}
