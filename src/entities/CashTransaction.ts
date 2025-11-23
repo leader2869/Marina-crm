5089f5466eb2edfc3718229607933ad2712878cb
Виталий Литвинюк <vitalymacbookair@MacBook-Air-Vitalij.local>
2025-11-23 21:21:13 +0300

    Временно закомментировано поле expenseCategoryId в entity CashTransaction до выполнения миграции БД

--- src/entities/CashTransaction.ts
+++ src/entities/CashTransaction.ts
@@ -72,11 +72,14 @@ export class CashTransaction {
   @Column({ nullable: true })
   categoryId: number | null;
 
-  @ManyToOne(() => VesselOwnerExpenseCategory, { nullable: true })
-  @JoinColumn({ name: 'expenseCategoryId' })
-  expenseCategory: VesselOwnerExpenseCategory | null;
-
-  @Column({ nullable: true })
-  expenseCategoryId: number | null;
+  // Временно закомментировано до выполнения миграции БД
+  // После выполнения миграции (создание таблицы vessel_owner_expense_categories и добавление поля expenseCategoryId)
+  // раскомментировать эти строки:
+  // @ManyToOne(() => VesselOwnerExpenseCategory, { nullable: true })
+  // @JoinColumn({ name: 'expenseCategoryId' })
+  // expenseCategory: VesselOwnerExpenseCategory | null;
+  //
+  // @Column({ nullable: true })
+  // expenseCategoryId: number | null;
 }
 
